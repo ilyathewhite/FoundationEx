adce81a955aa05cabdcdeae6b90422dcfce67142
Ilya Belenkiy <ilya.belenkiy@gmail.com>
2022-09-09 10:31:47 -0400

    Add warnings for slow compilation

--- Package.swift
+++ Package.swift
@@ -20,7 +20,13 @@ let package = Package(
     targets: [
         .target(
             name: "FoundationEx",
-            dependencies: [.product(name: "Tagged", package: "swift-tagged")]
+            dependencies: [.product(name: "Tagged", package: "swift-tagged")],
+            swiftSettings: [.unsafeFlags([
+                "-Xfrontend",
+                "-warn-long-function-bodies=100",
+                "-Xfrontend",
+                "-warn-long-expression-type-checking=100"
+            ])]
         )
     ]
 )
