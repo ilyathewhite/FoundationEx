e75ffb26d855c4e260f753fe466eb3c4cadc17f7
Ilya Belenkiy <ilya@harmonicsense.com>
2023-01-06 10:46:43 -0500

    Rename transform to mapAsSequence, add filterAsSequence

--- Sources/FoundationEx/AnySequence.swift
+++ Sources/FoundationEx/AnySequence.swift
@@ -6,12 +6,26 @@
 //
 
 public extension AnySequence {
-    func transform<T>(_ transform: @escaping (Element) throws -> T) rethrows -> AnySequence<T> {
+    func mapAsSequence<T>(_ transform: @escaping (Element) throws -> T) rethrows -> AnySequence<T> {
         let iterator = makeIterator()
         return AnySequence<T> {
             AnyIterator<T> {
                 guard let value = iterator.next() else { return nil }
                 return try? transform(value)
+            }
+        }
+    }
+
+    func filterAsSequence(_ isIncluded: @escaping (Element) -> Bool) -> AnySequence<Element> {
+        let iterator = makeIterator()
+        return AnySequence<Element> {
+            AnyIterator<Element> {
+                while let value = iterator.next() {
+                    if isIncluded(value) {
+                        return value
+                    }
+                }
+                return nil
             }
         }
     }
