3af6258072878c8287889b71e8af18afdc03deaf
Ilya Belenkiy <ilya.belenkiy@gmail.com>
2025-04-18 13:38:45 -0400

    Comment out unsafe flags for releases

--- Package.swift
+++ Package.swift
@@ -21,12 +21,14 @@ let package = Package(
         .target(
             name: "FoundationEx",
             dependencies: [.product(name: "Tagged", package: "swift-tagged")],
-            swiftSettings: [.unsafeFlags([
-                "-Xfrontend",
-                "-warn-long-function-bodies=100",
-                "-Xfrontend",
-                "-warn-long-expression-type-checking=100"
-            ])]
+            swiftSettings: [
+//                .unsafeFlags([
+//                    "-Xfrontend",
+//                    "-warn-long-function-bodies=100",
+//                    "-Xfrontend",
+//                    "-warn-long-expression-type-checking=100"
+//                ])
+            ]
         ),
         .testTarget(
             name: "FoundationExTests",
